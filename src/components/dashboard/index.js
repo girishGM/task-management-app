bdaa5938a1b3ad3d2f000aea0445512308c6d500
Girish Girish Narayan <girish.narayan@puresoft.int>
2021-08-10 18:08:50 +0800

    UI changes

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { Header } from "../header/header.component";
 import DashboardList from "./dashboard-list";
